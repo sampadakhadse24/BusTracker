598488710a8bc5ee9a50ad4ea2ab2fc90ce984cb
sampadakhadse24 <svpcet.iucee@gmail.com>
2024-10-23 15:39:26 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function redirect() {
   var busNumber = document.getElementById("bus-number").value.trim();
   if(busNumber !== "") {
-    // Assuming each bus has its own webpage named bus-X.html where X is the bus number
     window.location.href = "bus-" + busNumber + ".html";
   } else {
     alert("Please enter a valid bus number.");

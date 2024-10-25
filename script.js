22407d6f1bb7a732743a67395a9558a9b0970f07
sampadakhadse24 <svpcet.iucee@gmail.com>
2024-10-25 15:05:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function redirect() {
   var busNumber = document.getElementById("bus-number").value.trim();
-  if(busNumber !== "") {
-    window.location.href = "bus-" + busNumber + ".html";
+  if (["1", "2", "3", "4"].includes(busNumber)) {
+    window.location.href = `bus-map.html?busNumber=${busNumber}`;
   } else {
-    alert("Please enter a valid bus number.");
+    alert("Please enter a valid bus number (1, 2, 3, or 4).");
   }
 }
